dc67eedee6678cd6ce585db200b75fdd68d38917
Kan <secureis@gmail.com>
2024-03-13 03:28:59 +0900

    walltets container modify

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:app/widgets/button.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() {
   //var player = Player(name : 'Jone'); // Text("Hello Flutter")가 이처럼 position parameter로 들어가는 것이고..
@@ -19,77 +17,159 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color(0xFF1E1E1E), // colors가 아니다.
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          // .all 모두에게 적용, .only 특정 부분에만 적용, .symmetric 상하좌우에만 적용. 여백주는 것임
-          child: Column(
-            crossAxisAlignment:
-                CrossAxisAlignment.start, // Column의 자식들을 왼쪽으로 정렬
-            children: [
-              const SizedBox(
-                // 윗 공간 띄우기
-                height: 80,
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.end, // Row의 자식들을 오른쪽으로 정렬
-                // Row의 MainAxis는 수평방향이고, CrossAxis는 수직방향이다.
-                //Column의 MainAxis는 수직방향이고, CrossAxis는 수평방향이다.
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            // .all 모두에게 적용, .only 특정 부분에만 적용, .symmetric 상하좌우에만 적용. 여백주는 것임
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Column의 자식들을 왼쪽으로 정렬
+              children: [
+                const SizedBox(
+                  // 윗 공간 띄우기
+                  height: 80,
+                ),
+                const Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.end, // Row의 자식들을 오른쪽으로 정렬
+                  // Row의 MainAxis는 수평방향이고, CrossAxis는 수직방향이다.
+                  //Column의 MainAxis는 수직방향이고, CrossAxis는 수평방향이다.
 
-                children: [
-                  Column(
-                    crossAxisAlignment:
-                        CrossAxisAlignment.end, // Column의 자식들을 오른쪽으로 정렬
-                    children: [
-                      Text(
-                        'Hey, Kan',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
+                  children: [
+                    Column(
+                      crossAxisAlignment:
+                          CrossAxisAlignment.end, // Column의 자식들을 오른쪽으로 정렬
+                      children: [
+                        Text(
+                          'Hey, Kan',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          //color:Colors.white.withOpacity(0.8),
-                          color: Color.fromRGBO(255, 255, 255, 0.8),
-                          fontSize: 18,
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            //color:Colors.white.withOpacity(0.8),
+                            color: Color.fromRGBO(255, 255, 255, 0.8),
+                            fontSize: 18,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 120,
-              ),
-              Text('Total Balance',
-                  style: TextStyle(
-                      fontSize: 22, color: Colors.white.withOpacity(0.8))),
-              const SizedBox(
-                height: 5,
-              ),
-              Text('\$5 194 482',
-                  style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white.withOpacity(0.8))),
-              const SizedBox(height: 30),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween, // 사이 공간 주기
-                children: [
-                  // container는 div 같은 존재. 여기에는 child가 들어간다.
-                  Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xfff1b33b),
-                      textColor: Colors.black),
-                  Button(
-                      text: 'Request',
-                      bgColor: Color(0xff1F2123),
-                      textColor: Colors.white),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text('\$5 194 482',
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white.withOpacity(0.8))),
+                const SizedBox(height: 30),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween, // 사이 공간 주기
+                  children: [
+                    // container는 div 같은 존재. 여기에는 child가 들어간다.
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xfff1b33b),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xff1F2123),
+                        textColor: Colors.white),
+                  ],
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View all',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xff1f2123),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Euro',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  '6,426',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                const SizedBox(width: 5),
+                                Text(
+                                  'EUR',
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.8),
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
