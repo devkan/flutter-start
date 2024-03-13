ec1662c4ebed25d734570aec1dfd97ad6d486e70
Kan <secureis@gmail.com>
2024-03-14 01:42:44 +0900

    Refactor stateful.dart to use a list for numbers

--- app/lib/stateful.dart
+++ app/lib/stateful.dart
@@ -21,11 +21,15 @@ class App extends StatefulWidget {
 // state 클래스
 // 이건 counter라는 property를 가지고 있는 Dart 클래스일뿐이다. flutter가 아닌다.
 class _AppState extends State<App> {
-  int counter = 0; // 계속 데이타가 변해야 하기 때문에 final을 사용하지 않는다.
+  //int counter = 0; // 계속 데이타가 변해야 하기 때문에 final을 사용하지 않는다.
+
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      //counter = counter + 1;
+
+      numbers.add(numbers.length);
     });
     // seState를 사용해서 데이타의 변화가 생겼음을 알려줘야 한다.
     // setState()가 호출될때 매다 매번 아래의 build를 실행해서 ui를 변경해 주는 것이다. 매번..
@@ -51,10 +55,14 @@ class _AppState extends State<App> {
                 'Click count',
                 style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+
+              // for
+              for (var n in numbers) Text('$n'),
+
+              //Text(
+              //'$counter',
+              //  style: const TextStyle(fontSize: 30),
+              //),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
